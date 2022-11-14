eee672a160482eaa08f82d1b9f945f9a37f357e1
Shubham-techuz <shubham@techuz.com>
2022-11-14 18:03:13 +0530

    Made database cloud

--- db/config.js
+++ db/config.js
@@ -1,3 +1,6 @@
 const mongoose = require('mongoose');
 
-mongoose.connect('mongodb://localhost:27017/trello');
+// mongoose.connect('mongodb://localhost:27017/trello');
+
+
+mongoose.connect('mongodb+srv://shubham:root@cluster0.jridc0z.mongodb.net/trello?retryWrites=true&w=majority');
