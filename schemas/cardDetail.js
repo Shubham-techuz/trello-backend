dbe5cdb3922dcc3135c6e4d5aeb5f200bd10d1a1
Shubham-techuz <shubham@techuz.com>
2022-11-16 15:53:51 +0530

    cardDetail file Schema changed

--- schemas/cardDetail.js
+++ schemas/cardDetail.js
@@ -3,6 +3,7 @@ const mongoose = require('mongoose');
 const cardDetailSchema = new mongoose.Schema({
     cardId: String,
     cardName: String,
+    cardDescription: String,
     startTime: String,
     stopTime: String,
     totalTime: String
